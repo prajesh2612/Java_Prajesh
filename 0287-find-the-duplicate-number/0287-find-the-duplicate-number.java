5c04828d3a9c2d36b17546c808d8e812e337083b
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-10-16 00:26:21 +0530

    Time: 79 ms (5.04%), Space: 93.5 MB (8.39%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -34,5 +34,19 @@ class Solution {
             }
         }
         return p;
+        
+//         int l=nums.length;
+//         int sum1=0,sum2=0;
+//         for(int i=0;i<l;i++)
+//         {
+//             if(i<l-1)
+//             {
+//                 sum1=sum1+(i+1);
+//             }
+//             sum2=sum2+nums[i];
+//         }
+//         int diff=sum2-sum1;
+        
+//     return diff;
     }
 }
