52a88a32a33e0ea759faa245b2919a049eb7df12
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-10-15 21:44:21 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (92.23%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,33 +11,33 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-//           if (head == null) {
-//             return false;
-//         }
- 
-//         ListNode slow = head;
-//         ListNode fast = head.next;
-//         while (slow != fast) {
-//             if (fast == null || fast.next == null) {
-//                 return false;
-//             }
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//         return true;
-        Set<ListNode> set=new HashSet<>();
-        while(head!=null)
-        {
-            if(set.contains(head)==true)
-            {
-                return true;
-            }
-            else
-            {
-                set.add(head);
-                head=head.next;
-            }            
+          if (head == null) {
+            return false;
         }
-        return false;
+ 
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+        // Set<ListNode> set=new HashSet<>();
+        // while(head!=null)
+        // {
+        //     if(set.contains(head)==true)
+        //     {
+        //         return true;
+        //     }
+        //     else
+        //     {
+        //         set.add(head);
+        //         head=head.next;
+        //     }            
+        // }
+        // return false;
     }
 }
