3cf026ea3b226d788d1ec31f1361949960664de4
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-10-21 11:44:15 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (92.05%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,29 +1,38 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int l=nums.length;
-        for(int i=0;i<l;i++)
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        // int l=nums.length;
+        // for(int i=0;i<l;i++)
+        // {
+        //     if(hm.containsKey(nums[i])==true)
+        //     {
+        //         int of=hm.get(nums[i]);
+        //         int nf=of+1;
+        //         hm.put(nums[i],nf);
+        //     }
+        //     else
+        //     {
+        //         hm.put(nums[i],1);
+        //     }
+        // }
+        // Set<Integer> set=hm.keySet();
+        // for(int i:set)
+        // {
+        //     if(hm.get(i)==1)
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return 0;
+        
+        
+        //use the concept of xor
+        int result=0;
+        for(int i:nums)
         {
-            if(hm.containsKey(nums[i])==true)
-            {
-                int of=hm.get(nums[i]);
-                int nf=of+1;
-                hm.put(nums[i],nf);
-            }
-            else
-            {
-                hm.put(nums[i],1);
-            }
+            result^=i;
         }
-        Set<Integer> set=hm.keySet();
-        for(int i:set)
-        {
-            if(hm.get(i)==1)
-            {
-                return i;
-            }
-        }
-        return 0;
+        return result;
         
     }
 }
