8e6a5e4b39c58fd889f3ea3b12e29485ea2920fe
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-10-28 12:17:40 +0530

    Time: 4 ms (49.22%), Space: 43.9 MB (11.51%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,18 +1,46 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int maxP=Integer.MIN_VALUE;
-        int l=nums.length;
-        for(int i=0;i<(l-1);i++)
+    public static int getMax(List<Integer> list)
+    {
+        int max=Integer.MIN_VALUE;
+        int l=list.size();
+        int p=0;
+        for(int i=0;i<l;i++)
         {
-            for(int j=i+1;j<l;j++)
+            if(max<list.get(i))
             {
-                int pro=(nums[i]-1)*(nums[j]-1);
-                if(maxP<pro)
-                {
-                    maxP=pro;
-                }
+                max=list.get(i);
+                p=i;
             }
         }
-        return maxP;
+        return p;
+    }
+    public int maxProduct(int[] nums) {
+        // int maxP=Integer.MIN_VALUE;
+        // int l=nums.length;
+        // for(int i=0;i<(l-1);i++)
+        // {
+        //     for(int j=i+1;j<l;j++)
+        //     {
+        //         int pro=(nums[i]-1)*(nums[j]-1);
+        //         if(maxP<pro)
+        //         {
+        //             maxP=pro;
+        //         }
+        //     }
+        // }
+        // return maxP;
+        List<Integer> list=new ArrayList<>();
+        int l=nums.length;
+        for(int i=0;i<l;i++)
+        {
+            list.add(nums[i]);
+        }        
+        int maxP=getMax(list);
+        int max1=list.get(maxP);
+        list.remove(maxP);
+        maxP=getMax(list);
+        int max2=list.get(maxP);
+        int maxPro=(max1-1)*(max2-1);
+        return maxPro;
     }
 }
