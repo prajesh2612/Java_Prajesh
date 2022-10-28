2f8b1307b28a46da7ba8b2fd9823590ebf9ebd61
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-10-28 11:42:03 +0530

    Time: 64 ms (5.79%), Space: 54.5 MB (58.31%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -3,7 +3,7 @@ class Solution {
         int t1=0;
         String t2="";
         int l=names.length;
-        for(int i=0;i<l;i++)
+        for(int i=0;i<l-1;i++)
         {
             for(int j=0;j<l-1-i;j++)
             {
