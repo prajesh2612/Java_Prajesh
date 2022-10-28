617dc7b78d69e4cf92013a9d8eaaafa099e6da0a
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-10-28 11:41:25 +0530

    Time: 49 ms (11.84%), Space: 55 MB (15.83%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int t1=0;
+        String t2="";
+        int l=names.length;
+        for(int i=0;i<l;i++)
+        {
+            for(int j=0;j<l-1-i;j++)
+            {
+                if(heights[j]<heights[j+1])
+                {
+                    t1=heights[j];
+                    heights[j]=heights[j+1];
+                    heights[j+1]=t1;
+                    
+                    t2=names[j];
+                    names[j]=names[j+1];
+                    names[j+1]=t2;
+                }
+            }
+        }
+        return names;
+        
+    }
+}
