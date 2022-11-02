f195115b5fbaa8b8df724a34e6a19882ba6bf703
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-11-02 22:29:56 +0530

    Time: 5 ms (69.44%), Space: 56.3 MB (37.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,26 +1,45 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int n=nums.length,k=0;
-        for(int i=0;i<n;i++)
-        {
-            if(hm.containsKey(nums[i])==true)
-            {
-                int of=hm.get(nums[i]);
-                int nf=of+1;
-                hm.put(nums[i],nf);
-            }
-            else
-            {
-                hm.put(nums[i],1);
-            }
+        
+//         HashMap<Integer,Integer> hm=new HashMap<>();
+//         int n=nums.length,k=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(hm.containsKey(nums[i])==true)
+//             {
+//                 int of=hm.get(nums[i]);
+//                 int nf=of+1;
+//                 hm.put(nums[i],nf);
+//             }
+//             else
+//             {
+//                 hm.put(nums[i],1);
+//             }
             
-            if(hm.get(nums[i])>(n/2))
-            {
-                k=nums[i];
-                break;
+//             if(hm.get(nums[i])>(n/2))
+//             {
+//                 k=nums[i];
+//                 break;
+//             }
+//         }
+//         return k; 
+        
+         int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
             }
+            if(num==candidate)
+            count += 1; 
+            else count -= 1; 
         }
-        return k;        
+
+        return candidate;
+        
+        
+        
+        
     }
 }
