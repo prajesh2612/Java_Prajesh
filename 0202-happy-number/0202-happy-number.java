f88b514a6edf9a66aa7845f179625b7606f167e9
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-11-05 11:12:12 +0530

    Time: 1 ms (98.32%), Space: 39.1 MB (97.54%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> hs=new HashSet<>();
-        int sum=0;
-        while(hs.add(n))
+        Set<Integer> hs=new HashSet<>();//this hashset is used to store the sums of the digits in each iteration. As soon as the already exisiting sum in the hashset get added, that means there is a cycle and hence the number isn't a happy number
+        
+        while(n!=1)
         {
-            int n1=n;
-            sum=0;
+            int n1=n,sum=0;
             while(n1!=0)
             {
                 int d=n1%10;
                 sum=sum+(d*d);
                 n1=n1/10;
             }
-            if(sum==1)
+            if(hs.contains(sum))
             {
-                return true;
+                return false;
             }
+            hs.add(sum);
             n=sum;
         }
-        return false;        
+        return true;        
     }
 }
