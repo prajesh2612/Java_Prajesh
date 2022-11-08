71179329838422b1ca55ad3b129c6ea5d529f2fc
Prajesh Raj Singh <68776292+prajesh2612@users.noreply.github.com>
2022-11-08 17:47:18 +0530

    Time: 12 ms (53.90%), Space: 46.6 MB (10.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,39 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // int l=nums.length;
-        // int s=0,k=0;
-        // int a[]=new int[2];
-        // for(int i=0;i<l;i++)
-        // {
-        //     for(int j=i+1;j<l;j++)
-        //     {
-        //         s=nums[i]+nums[j];
-        //         if(s==target)
-        //         {
-        //             k=1;
-        //             a[0]=i;
-        //             a[1]=j;
-        //             break;
-        //         }
-        //     }
-        //     if(k==1)
-        //     {
-        //         break;
-        //     }
-        // }
-        // return(a);    
-        
-         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        int arr[] = new int[2];
-        for(int i = 0 ;i<nums.length; i++)
+       HashMap<Integer,Integer> hm=new HashMap<>();
+        int l=nums.length;
+        for(int i=0;i<l;i++)
         {
-            if(map.containsKey(target - nums[i]))
+            hm.put(nums[i],i);
+        }
+        int arr[]=new int[2];
+        for(int i=0;i<l;i++)
+        {
+            int diff=target-nums[i];
+            if((hm.containsKey(diff)==true)&&(hm.get(diff)!=i))
             {
-              arr[0] = i;
-              arr[1] = map.get(target - nums[i]); 
+                arr[0]=i;
+                arr[1]=hm.get(diff);
                 break;
             }
-            map.put(nums[i],i);
         }
         return arr;
     }
